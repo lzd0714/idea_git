3fbe86fe9dcb21884150a57e5939fe06943a7c14
LZD <1733775378@qq.com>
2022-06-25 14:20:00 +0800

    新建

---
+++ src/main/java/com/atguigu/Test.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @program: idea_git
+ * @author: LZD
+ * @create: 2022-06-25 10:44
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
